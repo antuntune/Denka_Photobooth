74f247f47fccb677ccd132a403f7b7532353c09c
MarkoKljajic1 <kljajicm1809@gmail.com>
2023-04-01 00:33:30 +0200

    Ociscen imports.py fajl

--- imports.py
+++ imports.py
@@ -1,42 +1,28 @@
-import cups
-from PyQt5.QtWidgets import QMainWindow
-from PyQt5 import uic, QtGui
+
+
+from PyQt5.QtCore import QThread, pyqtSignal, Qt
+from PyQt5.QtWidgets import QMainWindow, QComboBox, QApplication
+from PyQt5 import uic, QtGui, QtWidgets
 from PyQt5 import QtWidgets
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QImage
 from cloudinary.uploader import upload
 import json
 import time
 import cloudinary
-
-
-from PyQt5.QtWidgets import QMainWindow
-from PyQt5 import uic, QtGui
-from PyQt5 import QtWidgets
-from PyQt5.QtGui import QPixmap, QImage
-from PyQt5.QtCore import QThread, pyqtSignal, Qt
-import json
+import cups
 from PIL import Image
 
 import cv2
 import os
 import keyboard
 import queue
-
-from PyQt5.QtWidgets import QMainWindow, QComboBox
-from PyQt5 import uic
-from PyQt5 import QtWidgets
-from PyQt5.QtGui import QPixmap
-
 import pymongo
 import qrcode
-
-from PyQt5.QtWidgets import QApplication
-from PyQt5 import QtWidgets
 import sys
 import res
-
 import subprocess
 import importlib
+
 from splash import SplashUi
 from camera import CameraUi
 from print import PrintUi
@@ -45,15 +31,3 @@ from config import ConfigUi
 import os
 
 
-from PyQt5.QtWidgets import QMainWindow
-from PyQt5 import uic, QtGui
-from PyQt5 import QtWidgets
-from PyQt5.QtGui import QPixmap
-
-
-from PIL import Image
-
-from PyQt5.QtWidgets import QMainWindow
-from PyQt5 import uic
-from PyQt5 import QtWidgets
-from PyQt5.QtGui import QPixmap
